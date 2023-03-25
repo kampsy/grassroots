009940dd7f68a035bf00deff5606e492f73a5dff
kampsy <kampambachanda@gmail.com>
2023-03-25 09:49:40 +0200

    Add  table & button loader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import {
   Grid, GridItem, Box, VStack, Container, Button, Flex, Avatar,
-  Text,
+  Text, SimpleGrid, ButtonGroup, Skeleton, SkeletonText
 } from '@chakra-ui/react'
 import { HiHome } from 'react-icons/hi'
 
@@ -29,45 +29,68 @@ export default Home
 
 const Layout = () => {
   return (
-      <Grid
-        templateAreas={`'nav main'`}
-        gridTemplateRows={'auto'}
-        gridTemplateColumns={'250px auto'}  
-        h='100vh'
-        w={'100%'}
-        gap='0'
-        bg='black'
-        textColor={'white'}
-        fontWeight='bold'
-      >
-        <GridItem bg={'whiteAlpha.100'} area={'nav'}>
-          <Box p={4}>
-            <VStack spacing={5}>
-              <Box>
-                <Flex alignItems={'center'} gap={4}>
-                  <Avatar src='https://docs.grassecon.org/assets/logo.svg' />
-                  <Box>
-                    <Text className={inter.className} fontWeight='bold' bgClip='text' bgGradient='linear(to-r, green, blue)'>
-                      Grassroots Economics
-                    </Text>
-                  </Box>
-                </Flex>
-              </Box>
-              <Box w={'100%'}>
-                <Button w={'100%'} leftIcon={<HiHome />} colorScheme='blue' variant={'outline'}>
-                  Home
-                </Button>
-              </Box>
-            </VStack>
-          </Box>
-        </GridItem>
-        <GridItem area={'main'} bg="white" textColor={'black'} overflow='scroll'>
-          <Box p={4}>
-            <Container maxW={'4xl'}>
-              hello
-            </Container>
-          </Box>
-        </GridItem>
-      </Grid>
+    <Grid
+      templateAreas={`'nav main'`}
+      gridTemplateRows={'auto'}
+      gridTemplateColumns={'250px auto'}
+      h='100vh'
+      w={'100%'}
+      gap='0'
+      bg='black'
+      textColor={'white'}
+      fontWeight='bold'
+    >
+      <GridItem bg={'whiteAlpha.100'} area={'nav'}>
+        <Box p={4}>
+          <VStack spacing={5}>
+            <Box>
+              <Flex alignItems={'center'} gap={4}>
+                <Avatar src='https://docs.grassecon.org/assets/logo.svg' />
+                <Box>
+                  <Text className={inter.className} fontWeight='bold' bgClip='text' bgGradient='linear(to-r, green, blue)'>
+                    Grassroots Economics
+                  </Text>
+                </Box>
+              </Flex>
+            </Box>
+            <Box w={'100%'}>
+              <Button w={'100%'} leftIcon={<HiHome />} colorScheme='blue' variant={'outline'}>
+                Home
+              </Button>
+            </Box>
+          </VStack>
+        </Box>
+      </GridItem>
+      <GridItem area={'main'} bg="white" textColor={'black'} overflow='scroll'>
+        <Box p={4}>
+          <Container maxW={'4xl'}>
+            <AnimeTableSkeleton />
+          </Container>
+        </Box>
+      </GridItem>
+    </Grid>
+  )
+}
+
+const AnimeTableSkeleton = () => {
+  return (
+    <Box>
+      <SimpleGrid bg={'blackAlpha.50'} rounded='md' p={3} columns={2} spacing={2}>
+        <Box>
+          <SkeletonText noOfLines={21} spacing='4' skeletonHeight='4' endColor='gray.500' />
+        </Box>
+        <Box >
+          <SkeletonText noOfLines={21} spacing='4' skeletonHeight='4' endColor='blue.500' />
+        </Box>
+      </SimpleGrid>
+      <Box py={4}>
+        <Flex justifyContent={'end'}>
+          <ButtonGroup gap={2}>
+            <Skeleton w={'90px'} h='40px' rounded={'md'} />
+            <Skeleton w={'90px'} h='40px' rounded={'md'} />
+          </ButtonGroup>
+        </Flex>
+      </Box>
+    </Box>
   )
 }
