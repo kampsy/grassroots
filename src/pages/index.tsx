c786af0f9b2e2f17a51fd6286744233033249b84
kampsy <kampambachanda@gmail.com>
2023-03-25 09:41:03 +0200

    Typescript 5.0 breaks use 4.9.5 instead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,48 +28,46 @@ const Home = () => {
 export default Home
 
 const Layout = () => {
-
   return (
-    <Grid
-      templateAreas={`
-                  "nav main"`}
-      gridTemplateRows={'auto'}
-      gridTemplateColumns={'250px auto'}
-      h='100vh'
-      w={'100%'}
-      gap='0'
-      bg='black'
-      textColor={'white'}
-      fontWeight='bold'
-    >
-      <GridItem bg={'whiteAlpha.100'} area={'nav'}>
-        <Box p={4}>
-          <VStack spacing={5}>
-            <Box>
-              <Flex alignItems={'center'} gap={4}>
-                <Avatar src='https://docs.grassecon.org/assets/logo.svg' />
-                <Box>
-                  <Text className={inter.className} fontWeight='bold' bgClip='text' bgGradient='linear(to-r, green, blue)'>
-                    Grassroots Economics
-                  </Text>
-                </Box>
-              </Flex>
-            </Box>
-            <Box w={'100%'}>
-              <Button w={'100%'} leftIcon={<HiHome />} colorScheme='blue' variant={'outline'}>
-                Home
-              </Button>
-            </Box>
-          </VStack>
-        </Box>
-      </GridItem>
-      <GridItem area={'main'} bg="white" textColor={'black'} overflow='scroll'>
-        <Box p={4}>
-          <Container maxW={'4xl'}>
-            hello
-          </Container>
-        </Box>
-      </GridItem>
-    </Grid>
+      <Grid
+        templateAreas={`'nav main'`}
+        gridTemplateRows={'auto'}
+        gridTemplateColumns={'250px auto'}  
+        h='100vh'
+        w={'100%'}
+        gap='0'
+        bg='black'
+        textColor={'white'}
+        fontWeight='bold'
+      >
+        <GridItem bg={'whiteAlpha.100'} area={'nav'}>
+          <Box p={4}>
+            <VStack spacing={5}>
+              <Box>
+                <Flex alignItems={'center'} gap={4}>
+                  <Avatar src='https://docs.grassecon.org/assets/logo.svg' />
+                  <Box>
+                    <Text className={inter.className} fontWeight='bold' bgClip='text' bgGradient='linear(to-r, green, blue)'>
+                      Grassroots Economics
+                    </Text>
+                  </Box>
+                </Flex>
+              </Box>
+              <Box w={'100%'}>
+                <Button w={'100%'} leftIcon={<HiHome />} colorScheme='blue' variant={'outline'}>
+                  Home
+                </Button>
+              </Box>
+            </VStack>
+          </Box>
+        </GridItem>
+        <GridItem area={'main'} bg="white" textColor={'black'} overflow='scroll'>
+          <Box p={4}>
+            <Container maxW={'4xl'}>
+              hello
+            </Container>
+          </Box>
+        </GridItem>
+      </Grid>
   )
 }
